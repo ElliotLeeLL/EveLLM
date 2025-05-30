357390faa2091aa8725b539a517d41664c136043
ElliotLeeLL <xingyuli1996@gmail.com>
2025-05-30 11:18:26 +0800

    Add loss and accuracy calculation logic to fine_tune_classification.py

--- fine_tune_classification.py
+++ fine_tune_classification.py
@@ -1,10 +1,13 @@
 import torch
 import tiktoken
+from pathlib import Path
+from torch.utils.data import DataLoader
 
 from configuration import model_configs
 from model import EveLLMModel
-from utils.model_utils import load_weights_into_evellm_gpt, generate_top_k,text_to_token_ids, token_ids_to_text
+from utils.model_utils import *
 from utils.gpt_download import download_and_load_gpt2
+from dataset.eve_dataset import SpamDataset
 
 
 # Create a model with config
@@ -19,29 +22,101 @@ settings, params = download_and_load_gpt2(
 model_size="1558M", models_dir="gpt2"
 )
 load_weights_into_evellm_gpt(model, params)
+
+
+# Replace the output layer
+num_classes = 2
+model.out_head = torch.nn.Linear(
+    config["emb_dim"], num_classes
+)
 model.to(device)
 
-# Test the loaded model
+# Froze parameters for all layers except the last transformer block and the output layer
+for param in model.parameters():
+    param.requires_grad = False
+for param in model.transformer_blocks[-1].parameters():
+    param.requires_grad = True
+for param in model.final_norm.parameters():
+    param.requires_grad = True
+
 tokenizer = tiktoken.get_encoding("gpt2")
-# token_ids = generate_top_k(
-#     model=model,
-#     idx=text_to_token_ids("Life is beautiful", tokenizer=tokenizer).to(device),
-#     max_new_tokens=25,
-#     context_size=config["context_length"],
-#     top_k=50,
-#     temperature=1.5
-# )
+inputs = torch.tensor(tokenizer.encode("Do you have time")).unsqueeze(0)
+print(inputs)
+print(inputs.shape)
 
-for _ in range(10):
-    token_ids = generate_top_k(
-        model=model,
-        idx=text_to_token_ids("As far as I know", tokenizer=tokenizer).to(device),
-        max_new_tokens=25,
-        context_size=config["context_length"],
-        top_k=50,
-        temperature=1.5
-    )
+with torch.no_grad():
+    outputs = model(inputs.to(device))
+print(outputs)
+print(outputs.shape)
 
-    print(
-        "Output text: ", token_ids_to_text(token_ids, tokenizer=tokenizer)
+extracted_path = "sms_spam_collection"
+
+train_dataset = SpamDataset(
+    csv_file=Path(extracted_path) / "train.csv",
+    max_length=None,
+    tokenizer=tokenizer
+)
+
+val_dataset = SpamDataset(
+    csv_file=Path(extracted_path) / "validation.csv",
+    max_length=train_dataset.max_length,
+    tokenizer=tokenizer
+)
+
+test_dataset = SpamDataset(
+    csv_file=Path(extracted_path) / "test.csv",
+    max_length=train_dataset.max_length,
+    tokenizer=tokenizer
+)
+
+num_workers = 0
+batch_size = 8
+
+train_loader = DataLoader(
+    dataset=train_dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_workers,
+    drop_last=True
+)
+val_loader = DataLoader(
+    dataset=val_dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_workers,
+    drop_last=True
+)
+test_loader = DataLoader(
+    dataset=test_dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_workers,
+    drop_last=True
+)
+
+train_accuracy = calc_accuracy_loader(
+    train_loader, model, device, num_batches=10
+)
+val_accuracy = calc_accuracy_loader(
+    val_loader, model, device, num_batches=10
+)
+test_accuracy = calc_accuracy_loader(
+    test_loader, model, device, num_batches=10
+)
+print(f"Training accuracy: {train_accuracy*100:.2f}%")
+print(f"Validation accuracy: {val_accuracy*100:.2f}%")
+print(f"Test accuracy: {test_accuracy*100:.2f}%")
+
+with torch.no_grad():
+    train_loss = calc_loss_loader(
+        train_loader, model, device, num_batches=5
     )
+    val_loss = calc_loss_loader(
+        val_loader, model, device, num_batches=5
+    )
+    test_loss = calc_loss_loader(
+        test_loader, model, device, num_batches=5
+    )
+print(f"Training loss: {train_loss:.3f}")
+print(f"Validation loss: {val_loss:.3f}")
+print(f"Test loss: {test_loss:.3f}")
