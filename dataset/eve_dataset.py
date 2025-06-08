c1bc01b0c84e831a36acab7c61769365abf709aa
ElliotLeeLL <xingyuli1996@gmail.com>
2025-06-08 08:45:45 +0800

    Remove redundant code for eve_dataset.py

--- dataset/eve_dataset.py
+++ dataset/eve_dataset.py
@@ -23,91 +23,6 @@ class EveDataset(Dataset):
     def __getitem__(self, idx):
         return self.input_ids[idx], self.target_ids[idx]
 
-class SpamDataset(Dataset):
-    def __init__(
-            self, csv_file, tokenizer, max_length=None, pad_token_id=50256
-    ):
-        self.data = pd.read_csv(csv_file)
-        self.encoded_texts = [
-            tokenizer.encode(text)
-            for text in self.data["text"]
-        ]
-        if max_length is None:
-            self.max_length = self._longest_encoded_length()
-        else:
-            self.max_length = max_length
-            self.encoded_texts = [
-                encoded_text[:self.max_length]
-                for encoded_text in self.encoded_texts
-            ]
-
-        # Pad encoded texts to max length
-        self.encoded_texts = [
-            encoded_text + [pad_token_id] * (
-                    self.max_length - len(encoded_text)
-            )
-            for encoded_text in self.encoded_texts
-        ]
-
-    def __getitem__(self, index):
-        encoded = self.encoded_texts[index]
-        label = self.data.iloc[index]["label"]
-        return (
-            torch.tensor(encoded, dtype=torch.long),
-            torch.tensor(label, dtype=torch.long)
-        )
-
-    def __len__(self):
-        return len(self.data)
-
-    def _longest_encoded_length(self):
-        max_length = 0
-        for encoded_text in self.encoded_texts:
-            encoded_length = len(encoded_text)
-            if len(encoded_text) > max_length:
-                max_length = len(encoded_text)
-        return max_length
-
-
-def download_and_unzip_spam_data(
-        url, zip_path, extracted_path, data_file_path
-):
-    if data_file_path.exists():
-        print(f"Data file already exists at {data_file_path}. Skipping download.")
-        return
-
-    # Ensure the directory for the zip file exists
-    zip_path.parent.mkdir(parents=True, exist_ok=True)
-
-    with urllib.request.urlopen(url) as response:
-        with open(zip_path, "wb") as f:
-            f.write(response.read())
-
-    with zipfile.ZipFile(zip_path, "r") as zip_ref:
-        zip_ref.extractall(extracted_path)
-
-    original_file = Path(extracted_path) / "SMSSpamCollection"
-    os.rename(original_file, data_file_path)
-    print(f"Data file downloaded and unzipped to {data_file_path}.")
-
-
-def creat_balanced_classification_dataset(df):
-    num_spam = df[df["label"] == "spam"].shape[0]
-    ham_subset = df[df["label"] == "ham"].sample(
-        num_spam, random_state=43
-    )
-    balanced_df = pd.concat([
-        ham_subset,
-        df[df["label"] == "spam"]
-    ])
-    return balanced_df
-
-
-def map_classification_dataset(df):
-    df["label"] = df["label"].map({"ham": 0, "spam": 1})
-    return df
-
-
 def random_split(df, train_fraction, validation_fraction):
     df = df.sample(frac=1, random_state=43).reset_index(drop=True)
     train_end = int(len(df) * train_fraction)
@@ -118,23 +33,6 @@ def random_split(df, train_fraction, validation_fraction):
     test_df = df[validation_end:]
 
     return train_df, validation_df, test_df
-
-
-class EveDataset(Dataset):
-    def __init__(self, txt, tokenizer, max_length, stride):
-        self.input_ids = []
-        self.target_ids = []
-        token_ids = tokenizer.encode(txt, allowed_special={"<|endoftext|>"})
-        for i in range(0, len(token_ids) - max_length, stride):
-            self.input_ids.append(torch.tensor(token_ids[i:i + max_length]))
-            self.target_ids.append(torch.tensor(token_ids[i + 1:i + max_length + 1]))
-
-    def __len__(self):
-        return len(self.input_ids)
-
-    def __getitem__(self, idx):
-        return self.input_ids[idx], self.target_ids[idx]
-
 
 def create_dataloader(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
     tokenizer = tiktoken.get_encoding("gpt2")
