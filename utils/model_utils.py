748e24db9b8d9d3a924725761c4ac8e19b88229b
ElliotLeeLL <xingyuli1996@gmail.com>
2025-06-08 08:02:11 +0800

    Add format input functions for model_utils.py

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -249,20 +249,23 @@ def load_weights_into_evellm_gpt(model, params):
         model.out_head.weight, params["wte"]
     )
 
-def classify_review(
-  text, model, tokenizer, device, max_length=None, pad_token_id=50256
-):
-    model.eval()
-    input_ids = tokenizer.encode(text)
-    supported_context_length = model.position_embedding.weight.shape[1]
+def format_input_alpaca(entry):
+    instruction_text = (
+        f"Below is an instruction that describes a task. "
+        f"Write a response that appropriately completes the request. "
+        f"\n\n### Instruction:\n{entry['instruction']}"
+    )
 
-    input_ids = input_ids[:min(max_length, supported_context_length)]
+    input_text = (
+        f"\n\n### Input:\n{entry['input']}" if entry["input"] else ""
+    )
+    return instruction_text + input_text
 
-    input_ids += [pad_token_id] * (max_length - len(input_ids))
-
-    input_ids = torch.tensor(input_ids).to(device).unsqueeze(0)
-
-    with torch.no_grad():
-        logits = model(input_ids)[:, -1, :]
-    predicted_label = torch.argmax(logits, dim=-1).item()
-    return "spam" if predicted_label == 1 else "ham"
+def format_input_phi3(entry):
+    user_text = (
+        f"<|user|>"
+        f"\n{entry['instruction']} "
+        f"{entry['input']}" if entry["input"] else ""
+        f"\n\n"
+    )
+    return user_text
