5cecc35814ac5c1798fe76f98b4fc492b9075ebb
ElliotLeeLL <xingyuli1996@gmail.com>
2025-06-08 09:39:58 +0800

    Add InstructionDataset class and custom_collate_fn function to eve_dataset.py

--- dataset/eve_dataset.py
+++ dataset/eve_dataset.py
@@ -6,6 +6,7 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import tiktoken
+from utils.model_utils import format_input_alpaca
 
 
 class EveDataset(Dataset):
@@ -22,6 +23,58 @@ class EveDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.input_ids[idx], self.target_ids[idx]
+
+class InstructionDataset(Dataset):
+    def __init__(self, data, tokenizer):
+        self.data = data
+        self.encoded_texts = []
+        #
+        for entry in data:
+            instruction_alpaca = format_input_alpaca(entry)
+            response_text = f"\n\n### Response: \n{entry['output']}"
+            full_text = instruction_alpaca + response_text
+            self.encoded_texts.append(
+                tokenizer.encode(full_text)
+            )
+
+    def __getitem__(self, idx):
+        return self.encoded_texts[idx]
+
+    def __len__(self):
+        return len(self.data)
+
+
+def custom_collate_fn(
+    batch,
+    pad_token_id=50256,
+    ignore_index=-100,
+    allowed_max_length=None,
+    device="cpu"
+):
+    batch_max_length = max(len(item)+1 for item in batch)
+    inputs_lst, targets_lst = [], []
+    for item in batch:
+        new_item = item.copy()
+        new_item += [pad_token_id]
+        padded = (
+            new_item + [pad_token_id] * (batch_max_length - len(new_item))
+        )
+        inputs = torch.tensor(padded[:-1])
+        targets = torch.tensor(padded[1:])
+
+        mask = targets == pad_token_id
+        indices = torch.nonzero(mask).squeeze()
+        if indices.numel() > 1:
+            targets[indices[1:]] = ignore_index
+        if allowed_max_length is not None:
+            inputs = inputs[:allowed_max_length]
+            targets = targets[:allowed_max_length]
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.tensor(targets_lst).to(device)
+    return inputs_tensor, targets_tensor
 
 def random_split(df, train_fraction, validation_fraction):
     df = df.sample(frac=1, random_state=43).reset_index(drop=True)
