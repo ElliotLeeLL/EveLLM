13b9df1053bfe6335907f123a22f536c7389024a
ElliotLeeLL <xingyuli1996@gmail.com>
2025-06-11 08:16:30 +0800

    Reorganize code structure for model training files

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -266,3 +266,39 @@ def classify_review(
         logits = model(input_ids)[:, -1, :]
     predicted_label = torch.argmax(logits, dim=-1).item()
     return "spam" if predicted_label == 1 else "ham"
+
+def train_classifier_simple(
+    model, train_loader, val_loader, optimizer, device, num_epochs,eval_freq, eval_iter
+):
+    train_losses, val_losses, train_accuracies, val_accuracies = [], [], [], []
+    examples_num, global_step = 0, -1
+
+    for epoch in range(num_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            examples_num += input_batch.shape[0]
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device,eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                print(
+                    f"Ep {epoch + 1} (Step {global_step:06d}): "
+                    f"Train loss {train_loss:.3f}, "
+                    f"Val loss {val_loss:.3f}"
+                )
+
+        train_accuracy = calc_accuracy_loader(model=model, data_loader=train_loader, device=device, num_batches=eval_iter)
+        val_accuracy = calc_accuracy_loader(model=model, data_loader=val_loader, device=device, num_batches=eval_iter)
+        print(f"Training accuracy: {train_accuracy * 100:.2f}% | ", end="")
+        print(f"Validation accuracy: {val_accuracy * 100:.2f}%")
+        train_accuracies.append(train_accuracy)
+        val_accuracies.append(val_accuracy)
+    return train_losses, val_losses, train_accuracies, val_accuracies, examples_num
