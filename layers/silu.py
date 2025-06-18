12aeb50f99d90b5e514d11712c011ac01cf45321
ElliotLeeLL <xingyuli1996@gmail.com>
2025-06-18 10:23:08 +0800

    Add silu.py

---
+++ layers/silu.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+
+class SiLU(nn.Module):
+    def __init__(self):
+        super(SiLU, self).__init__()
+
+    def forward(self, x):
+        return x * torch.sigmoid(x)
