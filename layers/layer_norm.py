d126ac1e423db3a7fb2f5f26bd9b7f31e796e84b
ElliotLeeLL <xingyuli1996@gmail.com>
2025-06-18 09:23:54 +0800

    Add the RMSNorm class for layer_norm.py

--- layers/layer_norm.py
+++ layers/layer_norm.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from sympy.testing.pytest import tooslow
 
 
 class LayerNorm(nn.Module):
@@ -16,6 +17,18 @@ class LayerNorm(nn.Module):
         norm_x = (x - mean) / torch.sqrt(var + self.eps)
         return norm_x * self.scale + self.shift
 
+class RMSNorm(nn.Module):
+    def __init__(self, emb_dim, eps=1e-5):
+        super().__init__()
+        self.emb_dim = emb_dim
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(emb_dim)).float()
+
+    def forward(self, x):
+        mean = x.pow(2).mean(dim=-1, keepdim=True)
+        x_normed = x  * torch.rsqrt(mean + self.eps)
+        return (x_normed * self.weight).to(dtype=x.dtype)
+
 # # Test code
 # torch.set_printoptions(sci_mode=False)
 # batch_example = torch.randn(2, 3, 5)
@@ -25,3 +38,9 @@ class LayerNorm(nn.Module):
 # var_ln = ln_out.var(dim=-1, keepdim=True, unbiased=False)
 # print(mean_ln)
 # print(var_ln)
+
+# torch.manual_seed(123)
+# example_batch = torch.randn(2, 3, 4)
+# rms_norm = RMSNorm(emb_dim=example_batch.shape[-1])
+# rmsnorm_pytorch = torch.nn.RMSNorm(example_batch.shape[-1], eps=1e-5)
+# assert torch.allclose(rms_norm(example_batch), rmsnorm_pytorch(example_batch))
