ea4758d9a64f8a345892a2c30982d4a0ffde2a3d
ElliotLeeLL <xingyuli1996@gmail.com>
2025-07-02 16:12:00 +0800

    Update assign function for model_utils.py

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -149,104 +149,18 @@ def generate_top_k(
         idx = torch.cat([idx, idx_next], dim=1)
     return idx
 
-def assign(left, right):
+def assign(left, right, tensor_name="unknown"):
     if left.shape != right.shape:
-        raise ValueError(f"Shape mismatch. Left: {left.shape}, "
+        raise ValueError(f"Shape mismatch in tensor '{tensor_name}'. Left: {left.shape}, "
         "Right: {right.shape}"
         )
-    return torch.nn.Parameter(torch.tensor(right))
+    if isinstance(right, torch.Tensor):
+        return torch.nn.Parameter(right.clone().detach())
+    else:
+        return torch.nn.Parameter(torch.tensor(right))
 
-def load_weights_into_eve_llm_gpt(model, params):
-    model.position_embedding.weight = assign(
-        model.position_embedding.weight, params['wpe']
-    )
-    model.token_embedding.weight = assign(
-        model.token_embedding.weight, params['wte']
-    )
-
-    for b in range(len(params["blocks"])):
-        # Assign weights for attention layers
-        q_w, k_w, v_w = np.split(
-            (params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1
-        )
-        model.transformer_blocks[b].attention_layer.W_query.weight = assign(
-            model.transformer_blocks[b].attention_layer.W_query.weight, q_w.T
-        )
-        model.transformer_blocks[b].attention_layer.W_key.weight = assign(
-            model.transformer_blocks[b].attention_layer.W_key.weight, k_w.T
-        )
-        model.transformer_blocks[b].attention_layer.W_value.weight = assign(
-            model.transformer_blocks[b].attention_layer.W_value.weight, v_w.T
-        )
-
-        # Assign biases for attention layers
-        q_b, k_b, v_b = np.split(
-            (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1
-        )
-        model.transformer_blocks[b].attention_layer.W_query.bias = assign(
-            model.transformer_blocks[b].attention_layer.W_query.bias, q_b
-        )
-        model.transformer_blocks[b].attention_layer.W_key.bias = assign(
-            model.transformer_blocks[b].attention_layer.W_key.bias, k_b
-        )
-        model.transformer_blocks[b].attention_layer.W_value.bias = assign(
-            model.transformer_blocks[b].attention_layer.W_value.bias, v_b
-        )
-
-        # Assign weights and bias for output layers
-        model.transformer_blocks[b].attention_layer.output.weight = assign(
-            model.transformer_blocks[b].attention_layer.output.weight,
-            params["blocks"][b]["attn"]["c_proj"]["w"].T
-        )
-        model.transformer_blocks[b].attention_layer.output.bias = assign(
-            model.transformer_blocks[b].attention_layer.output.bias,
-            params["blocks"][b]["attn"]["c_proj"]["b"]
-        )
-
-        # Assign weights and bias for mlp layers
-        model.transformer_blocks[b].ff.layers[0].weight = assign(
-            model.transformer_blocks[b].ff.layers[0].weight,
-            params["blocks"][b]["mlp"]["c_fc"]["w"].T
-        )
-        model.transformer_blocks[b].ff.layers[0].bias = assign(
-            model.transformer_blocks[b].ff.layers[0].bias,
-            params["blocks"][b]["mlp"]["c_fc"]["b"]
-        )
-        model.transformer_blocks[b].ff.layers[2].weight = assign(
-            model.transformer_blocks[b].ff.layers[2].weight,
-            params["blocks"][b]["mlp"]["c_proj"]["w"].T
-        )
-        model.transformer_blocks[b].ff.layers[2].bias = assign(
-            model.transformer_blocks[b].ff.layers[2].bias,
-            params["blocks"][b]["mlp"]["c_proj"]["b"]
-        )
-
-        model.transformer_blocks[b].norm1.scale = assign(
-            model.transformer_blocks[b].norm1.scale,
-            params["blocks"][b]["ln_1"]["g"]
-        )
-        model.transformer_blocks[b].norm1.shift = assign(
-            model.transformer_blocks[b].norm1.shift,
-            params["blocks"][b]["ln_1"]["b"]
-        )
-        model.transformer_blocks[b].norm2.scale = assign(
-            model.transformer_blocks[b].norm2.scale,
-            params["blocks"][b]["ln_2"]["g"]
-        )
-        model.transformer_blocks[b].norm2.shift = assign(
-            model.transformer_blocks[b].norm2.shift,
-            params["blocks"][b]["ln_2"]["b"]
-        )
-
-    model.final_norm.scale = assign(
-        model.final_norm.scale, params["g"]
-    )
-    model.final_norm.shift = assign(
-        model.final_norm.shift, params["b"]
-    )
-    model.out_head.weight = assign(
-        model.out_head.weight, params["wte"]
-    )
+def load_weights_into_eve_llm_llama(model, param_config, params):
+    return
 
 def format_input_alpaca(entry):
     instruction_text = (
