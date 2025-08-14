65b441306c2fa00058ef9ded578d3ae240f71564
ElliotLeeLL <xingyuli1996@gmail.com>
2025-08-14 10:30:26 +0800

    Update transformer_block.py for eve-lla-qwen3

--- attention/transformer_block.py
+++ attention/transformer_block.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 from layers.feed_forward import GatedFeedForward
 from layers.layer_norm import RMSNormQwen
-from attention.multi_head_attention import GroupedQueryAttention
+from attention.grouped_query_attention import GroupedQueryAttention
 
 
 # class TransformerBlock(nn.Module):
@@ -63,19 +63,48 @@ from attention.multi_head_attention import GroupedQueryAttention
 #         x = shotcut + x
 #         return x
 
+# class TransformerBlock(nn.Module):
+#     def __init__(self, config):
+#         super().__init__()
+#         self.attention_layer = GroupedQueryAttention(
+#             d_in=config["emb_dim"],
+#             d_out=config["emb_dim"],
+#             num_heads=config["n_heads"],
+#             num_kv_groups=config["n_kv_groups"],
+#             dtype=config["dtype"],
+#         )
+#         self.ff = GatedFeedForward(config)
+#         self.norm1 = RMSNormQwen(config["emb_dim"])
+#         self.norm2 = RMSNormQwen(config["emb_dim"])
+#
+#     def forward(self, x, mask=None, cos=None, sin=None):
+#         shotcut = x
+#         x = self.norm1(x)
+#         x = self.attention_layer(
+#             x.to(torch.bfloat16), mask=mask, cos=cos, sin=sin
+#         )
+#         x = shotcut + x
+#
+#         shotcut = x
+#         x = self.norm2(x)
+#         x = self.ff(x)
+#         x = shotcut + x
+#         return x
+
 class TransformerBlock(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.attention_layer = GroupedQueryAttention(
             d_in=config["emb_dim"],
-            d_out=config["emb_dim"],
             num_heads=config["n_heads"],
+            head_dim=config["head_dim"],
             num_kv_groups=config["n_kv_groups"],
+            qk_norm=config["qk_norm"],
             dtype=config["dtype"],
         )
         self.ff = GatedFeedForward(config)
-        self.norm1 = RMSNormQwen(config["emb_dim"])
-        self.norm2 = RMSNormQwen(config["emb_dim"])
+        self.norm1 = RMSNormQwen(config["emb_dim"], eps=1e-6)
+        self.norm2 = RMSNormQwen(config["emb_dim"], eps=1e-6)
 
     def forward(self, x, mask=None, cos=None, sin=None):
         shotcut = x
