a15e562409f57f8fc4f1fe4f22784b376dea4cfa
ElliotLeeLL <xingyuli1996@gmail.com>
2025-08-20 09:00:44 +0800

    Add lora.py

---
+++ layers/lora.py
@@ -0,0 +1,29 @@
+import math
+import torch
+
+
+class LoRALayer(torch.nn.Module):
+    def __init__(self, in_dim, out_dim, rank, alpha):
+        super(LoRALayer, self).__init__()
+        self.A = torch.nn.Parameter(torch.empty(in_dim, rank))
+        torch.nn.init.kaiming_uniform_(self.A, a=math.sqrt(5))
+        self.B = torch.nn.Parameter(torch.zero(rank, out_dim))
+        self.alpha = alpha
+
+    def forward(self, x):
+        x = self.alpha * (x @ self.A @ self.B)
+        return x
+
+class LinearWithLoRA(torch.nn.Module):
+    def __init__(self, linear, rank, alpha):
+        super(LinearWithLoRA, self).__init__()
+        self.linear = linear
+        self.lora = LoRALayer(
+            linear.in_features,
+            linear.out_features,
+            rank,
+            alpha
+        )
+
+    def forward(self, x):
+        return self.linear(x) + self.lora(x)
