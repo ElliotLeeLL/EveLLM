70df3e0c1769f7178c7c42d7a4496a1d0e94af9f
ElliotLeeLL <xingyuli1996@gmail.com>
2025-08-22 08:59:12 +0800

    Update fine_tune_instruction.py

--- fine_tune_instruction.py
+++ fine_tune_instruction.py
@@ -37,6 +37,7 @@ if __name__ == "__main__":
     current_weights = load_file(weights_path)
     combined_weights.update(current_weights)
     load_weights_into_eve_llm_qwen3(model, config, combined_weights)
+    replace_linear_with_lora(model, rank=16, alpha=16, config=config)
     model.to(device)
 
     # Froze parameters for all layers except the last transformer block and the output layer
@@ -58,7 +59,7 @@ if __name__ == "__main__":
     tokenizer = Qwen3Tokenizer(
         str(tokenizer_file_path),
         add_generation_prompt=True,
-        add_thinking=True,
+        add_thinking=False,
     )
     file_path = Path("instruction_data") / "instruction_data.json"
     url = "https://raw.githubusercontent.com/tatsu-lab/stanford_alpaca/main/alpaca_data.json"
