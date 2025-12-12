7b4dadfbe8fdcdba6a6ccaf25ed59fc03d4a009e
ElliotLeeLL <xingyuli1996@gmail.com>
2025-12-12 21:06:43 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+from requests_toolbelt.multipart.encoder import total_len
+
 from attention.transformer_block import TransformerBlock
 from layers.layer_norm import RMSNormQwen, RMSNormGemma
 from utils.model_utils import compute_rope_params
@@ -126,7 +128,7 @@ from utils.model_utils import compute_rope_params
 #             cos, sin = compute_rope_params(
 #             head_dim=head_dim,
 #             theta_base=config["rope_base"],
-#             context_length=config["context_length"],
+#             context_drxrxdrfxxdrfxxecrdxcedyhry c xwelength=config["context_length"],
 #         )
 #         self.register_buffer("cos", cos, persistent=False)
 #         self.register_buffer("sin", sin, persistent=False)
@@ -186,15 +188,69 @@ class EveLLMModel(nn.Module):
         self.register_buffer("sin_global", sin_global, persistent=False)
         self.config = config
 
-    def forward(self, in_idx):
-        token_embeds = self.token_embedding(in_idx)
-        x = token_embeds
+    def _create_masks(
+            self,
+            cur_len,
+            device,
+            pos_start=0,
+            pos_end=None
+    ):
+        if pos_end is None:
+            pos_end = cur_len
+        total_len = pos_end
 
-        num_tokens = x.shape[1]
-        mask = torch.triu(torch.ones(num_tokens, num_tokens, device=x.device, dtype=torch.bool), diagonal=1)
+        ones = torch.ones((total_len, total_len), dtype=torch.bool, device=device)
 
-        for transformer_block in self.transformer_blocks:
-            x = transformer_block(x, mask=mask, cos=self.cos, sin=self.sin)
+        mask_global_full = torch.triu(ones, diagonal=1)
+
+        far_past_full = torch.triu(ones, diagonal=self.config["sliding_window"]).T
+
+        mask_local_full = mask_global_full | far_past_full
+
+        row_slice = slice(pos_start, pos_end)
+        mask_global = mask_global_full[row_slice, :pos_end][None, None, :, :]
+        mask_local = mask_local_full[row_slice, :pos_end][None, None, :, :]
+        return mask_global, mask_local
+
+    def forward(
+            self,
+            x,
+            input_ids,
+            cache=None
+    ):
+        b, seq_len = input_ids.shape
+        x = self.token_embedding(input_ids) * (self.config["emb_dim"] ** 0.5)
+
+        if cache is not None:
+            pos_start = self.current_pos
+            pos_end = pos_start + seq_len
+            self.current_pos = pos_end
+            mask_global, mask_local = self._create_masks(
+                cur_len=seq_len, device=x.device, pos_start=pos_start, pos_end=seq_len
+            )
+        else:
+            pos_start = 0
+            mask_global, mask_local = self._create_masks(
+                cur_len=seq_len, device=x.device, pos_start=0, pos_end=seq_len
+            )
+        for i, block in enumerate(self.transformer_blocks):
+            blk_cache = cache.get(i) if cache is not None else None
+            x, new_blk_cache = block(
+                x,
+                mask_global=mask_global,
+                mask_local=mask_local,
+                cos_global=self.cos_global,
+                sin_global=self.sin_global,
+                cos_local=self.cos_local,
+                sin_local=self.sin_local,
+                start_pos=pos_start,
+                cache=blk_cache,
+            )
+            if cache is not None:
+                cache.update(i, new_blk_cache)
         x = self.final_norm(x)
         logits = self.out_head(x.to(self.config["dtype"]))
         return logits
+
+    def reset_kv_cache(self):
+        self.current_pos = 0
