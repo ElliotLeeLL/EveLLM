fc72b0fcc2bfc01cc6c60860947d314c84f2bbc0
ElliotLeeLL <xingyuli1996@gmail.com>
2026-01-19 17:39:10 +0800

    Update gemma_tokenizer.py

--- tokenizer/gemma_tokenizer.py
+++ tokenizer/gemma_tokenizer.py
@@ -3,18 +3,20 @@ from pathlib import Path
 from tokenizers import Tokenizer
 
 class GemmaTokenizer:
-    def __init__(self, tokenizer_file_path):
+    def __init__(self, tokenizer_file_path: str):
         tok_file = Path(tokenizer_file_path)
-        self.tok_ = Tokenizer.from_file(str(tok_file))
+        self._tok = Tokenizer.from_file(str(tok_file))
+        # Attempt to identify EOS and padding tokens
         eos_token = "<end_of_turn>"
         self.pad_token_id = eos_token
         self.eos_token_id = eos_token
 
-    def encode(self, text):
-        return self.tok_.encode(text).ids
+    def encode(self, text: str) -> list[int]:
+        return self._tok.encode(text).ids
 
-    def decode(self, token_ids):
-        return self.tok_.decode(token_ids, skip_special_tokens=False)
+    def decode(self, ids: list[int]) -> str:
+        return self._tok.decode(ids, skip_special_tokens=False)
+
 
 def apply_chat_template(user_text):
     return f"<start_of_turn>user\n{user_text}<end_of_turn>\n<start_of_turn>model\n"
